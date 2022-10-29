4c1b9405a863c3bb3077ead2cfd435d77f322c05
Ade Setiawan <adesetiawan0675@gmail.com>
2022-10-29 20:19:25 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const navFixed = header.offsetTop; //nilai awal top: 0
+  const toTop = document.querySelector("#toTop");
 
   if (window.pageYOffset > navFixed) {
     header.classList.add("navbar-fixed");
+    toTop.classList.add("flex");
+    toTop.classList.remove("hidden");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -17,4 +22,12 @@ const navLink = document.querySelector("#nav-link");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navLink.classList.toggle("hidden");
+});
+
+//close hamburger in all area
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navLink) {
+    hamburger.classList.remove("hamburger-active");
+    navLink.classList.add("hidden");
+  }
 });
